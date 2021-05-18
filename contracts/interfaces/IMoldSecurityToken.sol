c6621bdc5234b8d1563bdf5fecfd05a3bf53fe94
Armen Arakelian <corporateaarakelian@gmail.com>
2021-05-18 21:06:15 +0300

    fixes

--- contracts/interfaces/IMoldSecurityToken.sol
+++ contracts/interfaces/IMoldSecurityToken.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity 0.7.6;
 
-import "./IERC20.sol";
 import "./IERC2612.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 interface IMoldSecurityToken is IERC20, IERC2612 {
     function deposit(uint256 value) external;
